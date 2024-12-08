90dadc8929adcfccc1bd7d1102c14373a8ae9e8c
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-08 10:40:20 +0530

    Learnt about deffer and it's flow

---
+++ 15(Deffer)/main.go
@@ -0,0 +1,21 @@
+// In this we will learn about defer keyword
+// In is basically used to manupliate the flow of the program
+// what ever we write with defer it is executed at the very last of the program
+// If multiple statements are there it follow's LIFO (last in frst out)
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Let's learn about defer in GO")
+	// check the order of printing
+	defer fmt.Println("G")
+	defer fmt.Println("A")
+	defer fmt.Println("N")
+	defer fmt.Println("L")
+	defer fmt.Println("-")
+	defer fmt.Println("O")
+	defer fmt.Println("G")
+	// LIFO order is printed
+}
