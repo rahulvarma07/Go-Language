7ffd50ce0d573a3b5e7167edb809a6467f60f31f
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-13 09:37:06 +0530

    Understod Json convert & converting Json

--- 22(JSON)/main.go
+++ 22(JSON)/main.go
@@ -17,7 +17,8 @@ type studentPortalDetais struct {
 
 func main() {
 	fmt.Println("Let's Understand about creating JSON data")
-	createJSON()
+	//createJSON()
+	JsonDecode()
 }
 
 func createJSON() {
@@ -27,7 +28,7 @@ func createJSON() {
 		{"Bob", "CS", 7.1, "ecap", []string{"CS-1", "CS-2", "CS-3"}},
 		{"Alice", "CSE", 9.1, "ecap", []string{"CSE-1", "CSE-2", "CSE-3"}},
 		{"Abram", "ECE", 8.1, "ecap", []string{"ECE-1", "ECE-2", "ECE-3"}},
-		{"Tate", "EEE", 8.1, "ecap", []string{"EEE-1", "EEE-2", "EEE-3"}},
+		{"Tate", "EEE", 8.1, "ecap", nil},
 	}
 
 	// To Convert the above data into JSON
@@ -46,4 +47,44 @@ func createJSON() {
 		panic(err)
 	}
 	fmt.Println(string(finalJsonData))
+
+	// Cross check the output to understand this..
+}
+
+func JsonDecode() {
+
+	jsonDataFromCall := []byte(`
+	{
+        "nameOfTheStudent": "Tate",
+        "branchOfTheStudent": "EEE",
+        "CGPA": 8.1
+    }
+	`)
+
+	// If we want to check that if our JSON data coming from web or any other req is valid or not
+	isValid := json.Valid(jsonDataFromCall)
+	if isValid {
+		fmt.Println("The JSON data is valid")
+	} else {
+		fmt.Println("THE JSON DATA IS NOT VALID")
+		return
+	}
+
+	// Storing JSON data coming into a strct
+	var StudentDetails1 studentPortalDetais
+	// Let's Say jsonDataFromCall(is the received data from call's) & we want to store it in a struct
+	json.Unmarshal(jsonDataFromCall, &StudentDetails1) // Store's it in a struct
+	// Remember to pass as refernce to avoid copy's
+	fmt.Printf("%#v\n", StudentDetails1)
+
+	// Now let's say you dont alway's want to store it in a struct
+	// you want to store it in a (Map){key : value}
+	var receivedData map[string]interface{}
+	json.Unmarshal(jsonDataFromCall, &receivedData)
+	//fmt.Printf("%#v\n", receivedData)
+
+	// Access or loop throught the map's
+	for key, val := range receivedData {
+		fmt.Printf("The Key is %v & the Val is %v \n", key, val)
+	}
 }
