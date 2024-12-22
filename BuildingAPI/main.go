f7247cae12b2d86fe2cde16a7554b7aca4088d0f
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-22 18:09:41 +0530

    Few more operation's learnt Using Fake DataBase (Seeding)

--- BuildingAPI/main.go
+++ BuildingAPI/main.go
@@ -3,23 +3,31 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Initially we did not work with DataBase
 // So lets assume these are model(structure) of our database..
+
+// Models - (file)
 type coureses struct {
 	CourseName  string       `json:"coursename"`
 	CourseID    string       `json:"course_id"`
 	CourseFee   float32      `json:"course_fee"`
-	CourseTutor *courseTutor `json:"course_tutor"`
+	CourseTutor *courseTutor `json:"course_tutor"` // Model interaction
 }
 
+// Model 2
 type courseTutor struct {
 	TutorName string `json:"tutor_name"`
 }
+
+// Model-end;
 
 // Fake DataBase
 var allCourses []coureses // Storing the model's inside a struct..
@@ -31,7 +39,8 @@ var allCourses []coureses // Storing the model's inside a struct..
 // name are not empty..
 
 func (course *coureses) validIDAndName() bool {
-	return (course.CourseName != "" && course.CourseID != "")
+	//return (course.CourseName != "" && course.CourseID != "") (commenting because i want to generate CourseID)
+	return course.CourseName != ""
 }
 
 func main() {
@@ -44,8 +53,45 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Building An API</h1>"))
 }
 
+// Sending all the courses available in DataBase
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get All Course")
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(allCourses)
+}
+
+// Sending only one course from database
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get One Course")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	// Iterationg and checking if the course is available or not..
+	for _, value := range allCourses {
+		if value.CourseID == params["id"] {
+			json.NewEncoder(w).Encode(value)
+			return
+		}
+	}
+
+	// If the course is no where to be found..
+	json.NewEncoder(w).Encode("No such course is found")
+}
+
+// Creating a new course and injecting into DB
+func createNewCourse(w http.ResponseWriter, r *http.Request) {
+	// Before injecting check for some condition's
+	if r.Body != nil {
+		json.NewEncoder(w).Encode("Enter some Data")
+	}
+
+	// What is user sends empty data {}
+	var course coureses
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if !course.validIDAndName() {
+		json.NewEncoder(w).Encode("Add some data")
+	}
+
+	rand.Seed()
 }
