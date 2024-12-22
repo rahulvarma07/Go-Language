4e00b3391d5f0537841ae9d9ccbe6f3d68642dfa
Rahul Varma Rudraraju <143200717+rahulvarma07@users.noreply.github.com>
2024-12-22 20:30:38 +0530

    Succesfully created API perfroming CRUD operatuions

--- BuildingAPI/main.go
+++ BuildingAPI/main.go
@@ -3,10 +3,13 @@
 package main
 
 import (
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -44,7 +47,40 @@ func (course *coureses) validIDAndName() bool {
 }
 
 func main() {
+	fmt.Println("Creating the routes")
 
+	// Creating a router
+	rout := mux.NewRouter()
+
+	// seeding..
+	allCourses = append(allCourses,
+		coureses{CourseName: "Go-Language", CourseID: "123", CourseFee: 0, CourseTutor: &courseTutor{TutorName: "Hithesh Choudary"}},
+		coureses{CourseName: "Flutter-Language", CourseID: "124", CourseFee: 35000, CourseTutor: &courseTutor{TutorName: "Vasanth"}},
+		coureses{CourseName: "DSA", CourseID: "125", CourseFee: 40000, CourseTutor: &courseTutor{TutorName: "Ashok"}},
+	)
+
+	// wrking with routes..
+
+	// First page of home
+	rout.HandleFunc("/", serveHome).Methods("GET")
+
+	// Get all courses available
+	rout.HandleFunc("/courses", getAllCourses).Methods("GET")
+
+	// Get only one course based on the id
+	rout.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+
+	// Create a new course
+	rout.HandleFunc("/addcourse", createNewCourse).Methods("POST")
+
+	// update one course
+	rout.HandleFunc("/updateonecourse", updateOneCourse).Methods("PUT")
+
+	// delete one course
+	rout.HandleFunc("delete/{id}", deleteACourse).Methods("DELETE")
+
+	// Listening to a port
+	log.Fatal(http.ListenAndServe(":7000", rout))
 }
 
 // When Opened a local host ensures there is some HTML code
@@ -93,5 +129,53 @@ func createNewCourse(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Add some data")
 	}
 
-	rand.Seed()
+	// Creating a random course ID
+	rand.NewSource(time.Now().UnixNano())
+	// Converting into string beacuse CourseId is of type string
+	randomId := strconv.Itoa(rand.Intn(10000))
+
+	// Setting the courseID
+	course.CourseID = randomId
+	allCourses = append(allCourses, course)
+
+	// Response
+	json.NewEncoder(w).Encode(course)
+}
+
+// Updating Course controller's
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("updating a course data")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// Update := (delete the exsisting course and add the new course)
+
+	for ind, cour := range allCourses {
+		if cour.CourseID == params["id"] {
+			// for deleting a particular index in slice
+			allCourses = append(allCourses[:ind], allCourses[ind+1:]...)
+
+			var course coureses
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseID = params["id"]
+			allCourses = append(allCourses, course)
+			json.NewEncoder(w).Encode(course)
+		}
+	}
+}
+
+// deleting a Course
+
+func deleteACourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("deleting a course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for ind, cour := range allCourses {
+		if cour.CourseID == params["id"] {
+			allCourses = append(allCourses[:ind], allCourses[ind+1:]...)
+			json.NewEncoder(w).Encode("Successfully deleted")
+		}
+	}
 }
